d77787aa7aca15a07f0fe47783444b6fcd740acf
Ryan Hamblin <rhamblin.contractor@lifevantage.com>
2015-05-18 09:45:26 -0600

    added addGame service

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,25 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
+	// This will add a new game
+	this.addNewGame = function(gameObj){
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		var deffered = $q.defer();
+		if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)){
+			gameObj.won = true;
+		}else if(parseInt(gameObj.homeTeamScore) <= parseInt(gameObj.opponentScore)){
+			gameObj.won = false;
+		};
+		$http.post({
+				url: url,
+				data: gameObj
+		}).then(function(response1){
+			deffered.resolve(response1);
+		});
+		return deffered.promise;
+	};
 
+
+
+// end service.
 });
